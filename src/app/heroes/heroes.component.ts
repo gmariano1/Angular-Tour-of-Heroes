f5f5a0fbd47c0d8d3af2ee95319acd368033f72b
Guilherme <gmariano1@live.com>
2018-09-28 14:17:44 -0300

    Atualizado heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
+	
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  
   constructor() { }
 
   ngOnInit() {
